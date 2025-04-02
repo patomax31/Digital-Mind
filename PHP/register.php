7ed992722758a0481e3d4f84be311c60704215c5
Ernesto <ehernandez79@ucol.mx>
2025-04-02 11:28:59 -0600

    Recuperar contraseña

--- PHP/register.php
+++ PHP/register.php
@@ -123,7 +123,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
 
                     <div class="forgot-password">
-                        <a href="#">¿Olvidaste tu contraseña?</a>
+                        <a href="Recuperar contraseña">¿Olvidaste tu contraseña?</a>
                     </div>
 
                     <p class="footer-text">&copy; Página desarrollada por DigitalMind</p>
