a88569b2669f34c180902fc38cba292770cadfd8
Ernesto <ehernandez79@ucol.mx>
2025-04-02 11:21:53 -0600

    Ernesto

--- PHP/includeHeader.js
+++ PHP/includeHeader.js
@@ -1,14 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const headerPlaceholder = document.createElement("div");
     headerPlaceholder.id = "header-placeholder";
 
+    
     document.body.insertBefore(headerPlaceholder, document.body.firstChild);
 
+<<<<<<< HEAD
     fetch("../PHP/header.html")
         .then(response => response.text())
+=======
+    
+    fetch("../PHP/header.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error al cargar el header");
+            }
+            return response.text();
+        })
+>>>>>>> 4cadc2d (Ernesto)
         .then(data => {
             headerPlaceholder.innerHTML = data;
         })
         .catch(error => console.error("Error al cargar el header:", error));
 });
-
