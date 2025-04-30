67d6bcb7a0be397b1c6c0430e8f9e6a91d99f5f6
friedman <amariz@ucol.mx>
2025-04-30 08:16:14 -0600

    carrusel chido

--- PHP/progress-bar.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load',() => {
-    const progress = document.getElementById('progress');
-    requestAnimationFrame(update);
-})
-
-function update(){
-    progress.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100)}%`; /*Para comillas invertidas es alt + 96*/ 
-    requestAnimationFrame(update);
-}
