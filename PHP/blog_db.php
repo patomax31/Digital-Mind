ce7b9430eb05afb004f647e9b72dcacd5c3b2413
Ernesto <ehernandez79@ucol.mx>
2025-05-07 07:35:59 -0600

    Marcianos

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -5,10 +5,10 @@ $pass = "";
 $db = "blog_db";
 
 // Create connection
-$conex = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 // Check connection
-if ($conex->connect_error) {
-    die("Connection failed: " . $conex->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
