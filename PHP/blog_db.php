09a83cf3edb74235203d573b5c6d14162c1a11d3
Ernesto <ehernandez79@ucol.mx>
2025-05-07 07:35:59 -0600

    Marcianos

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -6,10 +6,10 @@ $db_pass = ''; // XAMPP default is empty password
 $db_name = 'blog_db'; // Make sure this matches your actual database name
 
 // Create connection
-$conex = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 // Check connection
-if ($conex->connect_error) {
-    die("Connection failed: " . $conex->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
