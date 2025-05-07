60b7ad82bab6d3ed2c09edbc615af017c1529ad0
Ernesto <ehernandez79@ucol.mx>
2025-05-07 07:35:59 -0600

    Marcianos

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -4,30 +4,9 @@ $user = "root";
 $pass = "";
 $db = "blog_db";
 
-<<<<<<< HEAD
 $conn = new mysqli($host, $user, $pass, $db);
-=======
-// Create connection
-<<<<<<< HEAD
-<<<<<<< HEAD
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
->>>>>>> b2feb83 (Otra)
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
-=======
-$conex = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-// Check connection
-if ($conex->connect_error) {
-    die("Connection failed: " . $conex->connect_error);
->>>>>>> 293c6fe (Cambio)
-=======
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
->>>>>>> bdd14ab (Marcianos)
 }
 ?>
