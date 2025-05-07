525a00d40da7db02babf0d1cfbac3af454d7970f
Ernesto <ehernandez79@ucol.mx>
2025-05-07 07:35:59 -0600

    Marcianos

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -4,11 +4,8 @@ $db_user = 'root';
 $db_pass = ''; 
 $db_name = 'blog_db';
 
-<<<<<<< HEAD
-=======
 // Create connection
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
->>>>>>> 7dc266b (Marcianos)
 
 // Check connection
 if ($conn->connect_error) {
