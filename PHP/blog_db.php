22f20f60e992774feb45a9f0f8a6a520a3df2bb3
Ernesto <ehernandez79@ucol.mx>
2025-05-08 07:38:42 -0600

    Cambios

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -1,15 +1,8 @@
 <?php
-<<<<<<< HEAD
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = ''; 
-$db_name = 'blog_db';
-=======
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db = "blog_db";
->>>>>>> 58f5051 (Cambios)
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -17,7 +10,4 @@ if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 ?>
-<<<<<<< HEAD
-=======
-?>
->>>>>>> 58f5051 (Cambios)
+
