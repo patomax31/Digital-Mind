e35d42feb18ac63dbce5fd502723cce8ce35881f
Ernesto <ehernandez79@ucol.mx>
2025-05-09 13:45:07 -0600

    Cambio

--- PHP/register.php
+++ PHP/register.php
@@ -75,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="../css/login_style.css">
     <title>Digital Mind - Registro</title>
     <style>
         .ok { color: green; }
