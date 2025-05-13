ea9ded6db276051e1acb2222fc64ade1e12b9639
ZinedineHMC <zinehiramc@outlook.com>
2025-05-13 10:57:05 -0600

    Se agregan cambios simples como los archivos para la recuperacion de contraseña, ademas se modifico un poco un archivo que me daba error

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -1,15 +1,31 @@
 <?php
-// Database connection parameters
+// Parámetros de conexión
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = ''; // XAMPP default is empty password
-$db_name = 'blog_db'; // Make sure this matches your actual database name
+$db_pass = ''; // XAMPP usa contraseña vacía por defecto
+$db_name = 'blog_db';
 
-// Create connection
+// Crear conexión
 $conex = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Verificar conexión
+if ($conex->connect_error) {
+    die("Error de conexión: " . $conex->connect_error);
 }
 ?>
+
+//<?php
+// Database connection parameters
+//$db_host = 'localhost';
+//$db_user = 'root';
+//$db_pass = ''; // XAMPP default is empty password
+//$db_name = 'blog_db'; // Make sure this matches your actual database name
+
+// Create connection
+//$conex = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+// Check connection
+//if ($conex->connect_error) { 
+ //   die("Connection failed: " . $conex->connect_error);
+//}
+//?>*/
