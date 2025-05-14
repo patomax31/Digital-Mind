41afdea6ff4340800d291ae74116434d60490ea6
Ernesto <ehernandez79@ucol.mx>
2025-05-14 08:12:34 -0600

    cambis realizados

--- PHP/includeHeader.js
+++ PHP/includeHeader.js
@@ -5,11 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
     
     document.body.insertBefore(headerPlaceholder, document.body.firstChild);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     fetch("../PHP/header.html")
         .then(response => response.text())
-=======
+
     
     fetch("../PHP/header.html")
         .then(response => {
@@ -18,8 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             return response.text();
         })
->>>>>>> 4cadc2d (Ernesto)
-=======
+
     
     fetch("../PHP/header.html")
         .then(response => {
@@ -28,11 +26,10 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             return response.text();
         })
-=======
+
     fetch("../PHP/header.html")
         .then(response => response.text())
->>>>>>> fc9b2ff62cf04ec4456ca54ef5ca9afbfb4f9dfa
->>>>>>> 1757bcd3474208667417995af9ce4eacc58b127d
+
         .then(data => {
             headerPlaceholder.innerHTML = data;
         })
