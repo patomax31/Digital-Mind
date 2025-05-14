136f13b7b7031c7394d859a739049edb04252b17
Ernesto <ehernandez79@ucol.mx>
2025-05-14 08:39:19 -0600

    cambis realizados

--- PHP/header.php
+++ PHP/header.php
@@ -49,11 +49,16 @@
         <div class="search-container">
           <div class="pill-search">
             <div class="search-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
-                <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clip-rule="evenodd" />
-              </svg>
+              <img src="../images/icono de busqueda.png" alt="Buscar" width="20" height="20">
             </div>
-            <input type="text" class="search-input" placeholder="Buscar...">
+           <form action="buscar.php" method="GET" class="search-container">
+  <div class="pill-search">
+    <button type="submit" class="search-icon" style="background: none; border: none; padding: 0; cursor: pointer;">
+      <img src="../images/tu_icono_personalizado.png" alt="Buscar" width="20" height="20">
+    </button>
+    <input type="text" class="search-input" name="q" placeholder="Buscar...">
+  </div>2
+</form>
           </div>
         </div>
       </div>
