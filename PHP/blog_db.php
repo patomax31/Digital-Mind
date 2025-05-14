95cf237f921466768819b3b9f99ab830ca3013d9
Ernesto <ehernandez79@ucol.mx>
2025-05-14 13:19:43 -0600

    cambis realizados

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -1,8 +1,5 @@
 <?php
-<<<<<<< HEAD
-=======
 
->>>>>>> 94788d5 (cambis realizados)
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -14,7 +11,4 @@ if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 ?>
-<<<<<<< HEAD
-=======
 
->>>>>>> 94788d5 (cambis realizados)
