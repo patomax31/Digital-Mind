494e5d658eddabe8bb984af6c04d06b3199b169b
Ernesto <ehernandez79@ucol.mx>
2025-05-16 10:41:27 -0600

    Arreglos

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -5,10 +5,8 @@ $db_pass = '';
 $db_name = 'blog_db';
 
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+// Check connection
+if ($conex->connect_error) {
+    die("Connection failed: " . $conex->connect_error);
 }
 ?>
