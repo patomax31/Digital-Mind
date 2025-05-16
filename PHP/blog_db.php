221f60dab632d75a88928132986767ccf7b15427
Ernesto <ehernandez79@ucol.mx>
2025-05-15 22:23:36 -0600

    Arreglos

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -4,7 +4,12 @@ $user = "root";
 $pass = "";
 $db = "blog_db";
 
+<<<<<<< HEAD
 $conn = new mysqli($host, $user, $pass, $db);
+=======
+// Create connection
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+>>>>>>> b2feb83 (Otra)
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
