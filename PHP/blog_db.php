8b9b5d8f31d9b366a0765fc9f8cdb01f595958ef
Ernesto <ehernandez79@ucol.mx>
2025-05-15 22:07:18 -0600

    Bajados

--- PHP/blog_db.php
+++ PHP/blog_db.php
@@ -7,6 +7,6 @@ $db = "blog_db";
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error); 
 }
 ?>
