6c8bd58eae111da637d41c7ad77b3623720d6282
Ernesto <ehernandez79@ucol.mx>
2025-05-16 11:10:47 -0600

    Arreglos

--- PHP/includeHeader.js
+++ PHP/includeHeader.js
@@ -5,13 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     
     document.body.insertBefore(headerPlaceholder, document.body.firstChild);
 
-
-    fetch("../PHP/header.html")
-        .then(response => response.text())
-
-    
-
-
     
     fetch("../PHP/header.html")
         .then(response => {
@@ -20,18 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             return response.text();
         })
-
-    
-    fetch("../PHP/header.html")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Error al cargar el header");
-            }
-            return response.text();
-        })
-
-    fetch("../PHP/header.html")
-        .then(response => response.text())
 
         .then(data => {
             headerPlaceholder.innerHTML = data;
